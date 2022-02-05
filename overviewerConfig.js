dc7922a0d8596841713b8c297b0b8c280775f0f0
Leo von Ahn <leotoyra@gmail.com>
2022-02-05 13:42:05 +0100

    Initial commit

---
+++ overviewerConfig.js
@@ -0,0 +1,98 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Toyboys - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1644064658",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Toyboys",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "toyboys",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Toyboys - overworld",
+            "last_rendertime": 1644063806,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -50,
+                135,
+                12
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -50,
+                135,
+                12
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Toyboys Caves",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "toyboyscaves",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Toyboys - overworld",
+            "last_rendertime": 1644063806,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -50,
+                135,
+                12
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -50,
+                135,
+                12
+            ],
+            "north_direction": 0
+        }
+    ]
+};
